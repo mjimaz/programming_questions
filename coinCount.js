9f8276347eefaa29799532c53e37daec2d9e37ec
mjimaz <m.aljimaz@gmail.com>
2016-06-07 11:30:39 -0700

    implement coin count

---
+++ coinCount.js
@@ -0,0 +1,17 @@
+function coinCount(n, currentIndex = 0, count=0){
+  let coins = [25, 10, 5, 1];
+  let currentCoin = coins[currentIndex];
+  if(n === 0){
+    return 1;
+  }
+  for(let i = 0; n-i*currentCoin >= 0 && currentIndex < coins.length; i++){
+    count += coinCount(n-i*currentCoin, currentIndex+1);
+  }
+  return count;
+}
+
+console.log(coinCount(1));
+console.log(coinCount(2));
+console.log(coinCount(5));
+console.log(coinCount(10));
+console.log(coinCount(100));
